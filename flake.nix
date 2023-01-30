5d8f98593ca555a49267fdb24e9b49e473d7693b
Joseph Price <pricejosephd@gmail.com>
2023-01-29 23:13:44 -0500

    fix static linking

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
   inputs.nix-filter.url = "github:numtide/nix-filter";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.inputs.flake-utils.follows = "flake-utils";
-  inputs.nixpkgs.url = "github:nix-ocaml/nix-overlays";
+  #inputs.nixpkgs.url = "github:nix-ocaml/nix-overlays";
+  inputs.nixpkgs.url = "/home/josephp/dev/nix-overlays";
 
   outputs = { self, nixpkgs, flake-utils, nix-filter }:
     flake-utils.lib.eachDefaultSystem (system:
